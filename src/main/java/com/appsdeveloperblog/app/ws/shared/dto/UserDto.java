54de1533912e0d370568b4d2bcb732a07ce01b3c
Rory Demers <roryjackd@yahoo.com>
2019-02-13 14:18:38 -0700

    Implemented service class method

--- src/main/java/com/appsdeveloperblog/app/ws/shared/dto/UserDto.java
+++ src/main/java/com/appsdeveloperblog/app/ws/shared/dto/UserDto.java
@@ -3,15 +3,71 @@ package com.appsdeveloperblog.app.ws.shared.dto;
 import java.io.Serializable;
 
 public class UserDto implements Serializable {
-
+	
+	private static final long serialVersionUID = 2872358563615601300L;
 	private long id;		
+	private String userId;
 	private String firstname;
 	private String lastname;
 	private String email;
 	private String password;
 	private String encryptedPassword;
 	private String emailVerificationToken;
-	private String emailVerificationStatus;
+	private Boolean emailVerificationStatus;
 
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEncryptedPassword() {
+		return encryptedPassword;
+	}
+	public void setEncryptedPassword(String encryptedPassword) {
+		this.encryptedPassword = encryptedPassword;
+	}
+	public String getEmailVerificationToken() {
+		return emailVerificationToken;
+	}
+	public void setEmailVerificationToken(String emailVerificationToken) {
+		this.emailVerificationToken = emailVerificationToken;
+	}
+	public Boolean getEmailVerificationStatus() {
+		return emailVerificationStatus;
+	}
+	public void setEmailVerificationStatus(Boolean emailVerificationStatus) {
+		this.emailVerificationStatus = emailVerificationStatus;
+	}
 
 }
