fbf23d5daddb10ffd2db69d9c7dfebf70ed59207
Rory Demers <roryjackd@yahoo.com>
2019-02-17 11:39:12 -0700

    Refactor duplicate entry.

--- src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceimpl.java
+++ src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceimpl.java
@@ -18,6 +18,8 @@ public class UserServiceimpl implements UserService {
 	@Override
 	public UserDto createUser(UserDto user) {
 		
+		if(userRepository.findByEmail(user.getEmail()) != null ) throw new RuntimeException("Record already exists");
+		
 		UserEntity userEntity = new UserEntity();
 		BeanUtils.copyProperties(user, userEntity);
 		
